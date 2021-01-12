7c4efc34896686f17908d7b6d33ee15ac2b733fc
adoestreito <andresdoestreito@gmail.com>
2021-01-12 19:49:59 +0100

    Select multiple stocks at the same time

--- SimpleStock.py
+++ SimpleStock.py
@@ -1,26 +1,15 @@
 import yfinance as yf
 import streamlit as st
 
-def tickerSelected(tickerSymbol):
-    #get data on this ticker
-    tickerData = yf.Ticker(tickerSymbol)
-    with st.beta_expander("Company Summary"):
-        col1, col2, = st.beta_columns([1,4])
-        with col1:
-            st.image(tickerData.info['logo_url'], use_column_width=True)
-        with col2:
-            st.write(tickerData.info['longBusinessSummary'])
-    #get the historical prices for this ticker
-
-    tickerDf = tickerData.history('max')
-    # Open	High	Low	Close	Volume	Dividends	Stock Splits
-    st.area_chart(tickerDf.Close)
-    st.line_chart(tickerDf.Volume)
-    st.line_chart(tickerDf.Dividends)
-
 def allSelected(tickerSymbol): 
-        #get data on this ticker
-    tickerData = yf.Tickers(tickerSymbol)
+    if len(tickerSymbol)==0:
+        st.write("Use the menu on the left to select the stocks that you want to compare")
+        return
+    elif len(tickerSymbol) == 1:
+        tickerData = yf.Ticker(tickerSymbol[0])
+        
+    else:
+        tickerData = yf.Tickers(tickerSymbol)
     tickerDf = tickerData.history('max')
     # Open	High	Low	Close	Volume	Dividends	Stock Splits
     st.write("""
@@ -54,14 +43,23 @@ tickersOptions= [
     },
 ]
 stockNames = []
+selectedTicker = []
+
 #Iterate to get all the tickerNames
 for option in tickersOptions:
     stockNames.append(option['stockName'])
 
-option = st.sidebar.selectbox('Select your ticket',stockNames)
-for stock in tickersOptions:
-    if option == stock['stockName']:
-        tickerSelected(stock['ticker'])
+#Multiple selection of tickers
+options = st.sidebar.multiselect(
+   'Select your tickets',stockNames)
+for choice in options:
+   for stock in tickersOptions:
+    if choice == stock['stockName']:
+        selectedTicker.append(stock['ticker'])
+
+allSelected(selectedTicker)
+
+
 
 
 
